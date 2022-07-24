79ad18f67cf82026926c33ec299fd9c10aae68a3
CXwudi <cx2010017@gmail.com>
2022-07-23 20:13:03 -0400

    add new runCmd() for input list of string

--- lib/src/main/kotlin/mikufan/cx/executil/ProcessBuilderUtil.kt
+++ lib/src/main/kotlin/mikufan/cx/executil/ProcessBuilderUtil.kt
@@ -2,8 +2,23 @@ package mikufan.cx.executil
 
 import java.nio.file.Path
 
+/**
+ * Start running a command line that is customized by a given [ProcessBuilder]
+ * @param command Array<out String> command line
+ * @param builder [@kotlin.ExtensionFunctionType] Function1<ProcessBuilder, Unit> process builder used to custom the process
+ * @return Process a running process, which the [ProcessBuilder.start] method is already called
+ */
 inline fun runCmd(vararg command: String, builder: ProcessBuilder.() -> Unit = {}): Process =
   ProcessBuilder(*command).apply(builder).start()
+
+/**
+ * Start running a command line that is customized by a given [ProcessBuilder]
+ * @param command Collection<out String> command line
+ * @param builder [@kotlin.ExtensionFunctionType] Function1<ProcessBuilder, Unit> process builder used to custom the process
+ * @return Process a running process, which the [ProcessBuilder.start] method is already called
+ */
+inline fun runCmd(command: Collection<out String>, builder: ProcessBuilder.() -> Unit = {}): Process =
+  runCmd(*command.toTypedArray(), builder = builder)
 
 var ProcessBuilder.directory: Path
   get() = this.directory().toPath()
@@ -37,3 +52,7 @@ var ProcessBuilder.redirectInput: ProcessBuilder.Redirect
 
 val ProcessBuilder.environment: MutableMap<String, String>
   get() = this.environment()
+
+fun ProcessBuilder.addMoreEnvironment(newEnvironment: Map<String, String>) {
+  environment.putAll(newEnvironment)
+} 
